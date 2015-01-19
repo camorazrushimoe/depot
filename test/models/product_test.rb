cb41c7a58821719361d1f1e83eb5270b6c521614
Sergey <biginf.com@gmail.com>
2015-01-19 15:39:47 +0300

    add block test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	  test "product attributes must not be empty" do
+		# свойства товара не должны оставаться пустыми 
+		product = Product.new
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any? 
+		assert product.errors[:price].any?
+		assert product.errors[:image_url].any?
+	end
+
+
+	test "product price must be positive" do
+	# цена товара должна быть положительной
+	product = Product.new(title: "My Book Title",
+	                        description: "yyy",
+	                        image_url:     "zzz.jpg")
+	    product.price = -1
+	    assert product.invalid?
+	    assert_equal ["must be greater than or equal to 0.01"],
+	    product.errors[:price]
+	          # должна быть больше или равна 0.01
+	    product.price = 0
+	    assert product.invalid?
+	    assert_equal ["must be greater than or equal to 0.01"],
+	        product.errors[:price]
+	    product.price = 1
+	    assert product.valid?
+	end
 end
+
